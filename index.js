fbbff9b1351851ace8e048213a161d20ebf3630f
SumiaAwad <159062215+SumiaAwad@users.noreply.github.com>
2024-03-14 16:23:45 -0400

    part one

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // import "./style.css";
+//1
 let mainEl = document.querySelector('main');
-mainEl.style.backgroundColor = '#4a4e4d';
+//2
+mainEl.style.backgroundColor = "var(--main-bg)";
+//3 
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+//4
+mainEl.classList.add("flex-ctr");
+//part two
+//1
+let topMenuEl = document.querySelector('nav');
+//2
+topMenuEl.style.height = '100%';
+//3
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+//4
+topMenuEl.classList.add("flex-around");
