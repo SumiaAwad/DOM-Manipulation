46a4260a610a1651cf0d563bac82f2ef54e183e2
SumiaAwad <159062215+SumiaAwad@users.noreply.github.com>
2024-03-15 14:51:43 -0400

    last version

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// import "./style.css";
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
 //1
 let mainEl = document.querySelector('main');
 //2
@@ -16,3 +22,16 @@ topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 //4
 topMenuEl.classList.add("flex-around");
+//part three
+// Iterate over the entire menuLinks array and for each "link" object:
+// Create an <a> element.
+// On the new element, add an href attribute with its value set to the href property of the "link" object.
+// Set the new element's content to the value of the text property of the "link" object.
+// Append the new element to the topMenuEl element.
+//Iterate over the entire menuLinks
+for (i = 0; i < menuLinks.length; i++) {
+    const aTag = document.createElement('a')
+    aTag.setAttribute('href', menuLinks[i].href);
+    aTag.innerText = menuLinks[i].text;
+    document.getElementById("top-menu").appendChild(aTag);
+}
